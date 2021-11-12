6c9123d0b343dcfa913a9328ef32c38600f35cab
NourG8 <nourgerfali08@gmail.com>
2021-11-12 21:55:24 +0100

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,85 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import 'model/products_repository.dart';
 import 'model/product.dart';
+import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-// TODO: Make a collection of cards (102)
-
-// Replace this entire method
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-
-    if (products.isEmpty) {
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-        locale: Localizations.localeOf(context).toString());
-
-    return products.map((product) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        // TODO: Adjust card heights (103)
-        elevation: 0.0,
-        child: Column(
-          // TODO: Center items on the card (103)
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18 / 11,
-              child: Image.asset(
-                product.assetName,
-                package: product.assetPackage,
-                // TODO: Adjust the box size (102)
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  // TODO: Align labels to the bottom and center (103)
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  // TODO: Change innermost Column (103)
-                  children: <Widget>[
-                // TODO: Handle overflowing labels (103)
-                    Text(
-                      product.name,
-                      style: theme.textTheme.button,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                    const SizedBox(height: 4.0),
-                    Text(
-                      formatter.format(product.price),
-                      style: theme.textTheme.caption,
-                    ),
-                    // End new code
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }).toList();
-  }
   // TODO: Add a variable for Category (104)
+
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
       appBar: AppBar(
-        // TODO: Add buttons and title (102)
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
         leading: IconButton(
           icon: const Icon(
             Icons.menu,
@@ -90,7 +24,6 @@ class HomePage extends StatelessWidget {
           },
         ),
         title: const Text('SHRINE'),
-        // TODO: Add trailing buttons (102)
         actions: <Widget>[
           IconButton(
             icon: const Icon(
@@ -103,8 +36,8 @@ class HomePage extends StatelessWidget {
           ),
           IconButton(
             icon: const Icon(
-              Icons.tune,
-              semanticLabel: 'filter',
+                Icons.tune,
+                semanticLabel: 'filter'
             ),
             onPressed: () {
               print('Filter button');
@@ -112,14 +45,7 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.count(
-          crossAxisCount: 2,
-          padding: const EdgeInsets.all(16.0),
-          childAspectRatio: 8.0 / 9.0,
-          children: _buildGridCards(context) // Changed code
-      ),
-      // TODO: Set resizeToAvoidBottomInset (101)
-      resizeToAvoidBottomInset: false,
+      body: AsymmetricView(products: ProductsRepository.loadProducts(Category.all)),
     );
   }
 }
